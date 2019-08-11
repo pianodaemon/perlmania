6ea95586161293e67cbf1fd7ea37da8f5aeb28b3
Rafael Diaz de Leon <leon@elinter.net>
2019-08-11 07:37:47 -0500

    Check stages on count for project/with_follow_ups

--- obras/service/genl/endpoints/projects.py
+++ obras/service/genl/endpoints/projects.py
@@ -2,7 +2,7 @@ import json
 
 import dal.project
 from dal.follow_ups import project_follow_ups_model
-from flask import request
+from flask import current_app, request
 from flask_restplus import Resource
 from genl.restplus import api
 from misc.helper import get_search_params
@@ -169,7 +169,6 @@ class ProjectsWithFollowUpCount(Resource):
                 "category",
                 "department",
                 "city",
-                "check_stage",
                 "adjudication",
                 "funding",
                 "program",
@@ -180,6 +179,15 @@ class ProjectsWithFollowUpCount(Resource):
                 "contract_end_date",
             ],
         )
+
+        # We should be able to filter by multiple check_stage values
+        check_stages = request.args.getlist("check_stage")
+
+        if search_params and check_stages:
+            search_params["check_stage"] = check_stages
+        elif check_stages:
+            search_params = {"check_stage": check_stages}
+
         count = dal.project.paged_with_follow_ups_count(search_params, empty_follow_ups)
 
         return {"count": count}
